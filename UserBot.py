b331ae153776c36e794d3965a2d97036df1303da
FALAHPSG1 <apalahpoph@gmail.com>
2024-10-29 00:55:40 +0700

    Update UserBot.py

--- UserBot.py
+++ UserBot.py
@@ -3,8 +3,8 @@ import os
 import asyncio
 from datetime import datetime
 
-api_id = '29798494'
-api_hash = '53273c1de3e68a9ecdb90de2dcf46f6c'
+api_id = '6477761695'
+api_hash = '7635162757:AAHnlI4xw1lkLL9Wltj_ipk-ThMlKVLLob8'
 
 client = TelegramClient('userbot', api_id, api_hash)
 device_owner_id = None
